5bcf9622b8c8bbdf415f93ffbe7bbe13956538c2
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-04 13:40:25 -0800

    Generate User model

---
+++ db/migrate/20180104213933_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration[5.1]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.integer :phone_number
+      t.string :password
+      t.integer :zipcode
+      t.string :state
+      t.string :city
+      t.string :address
+      t.integer :ssn
+      t.integer :fee
+      t.text :description
+      t.string :avatar
+      t.string :verification_image
+
+      t.timestamps
+    end
+  end
+end
