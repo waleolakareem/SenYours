5dabd019943eeafa6c7e8bca559d8b83c2ef9995
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-04 15:22:57 -0800

    Delete rails g routes and input resources :user to use all user routes necessary in the application

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/edit'
-
-  get 'users/destroy'
-
+  resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
