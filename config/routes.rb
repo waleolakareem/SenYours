1e6e9c8117ed618ca89a261eecec7b9ba634743f
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-04 15:10:34 -0800

    Generate controller user to write authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
