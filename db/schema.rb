403208ebaf22a76a7c40aa3801ad3bf788dfc06c
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-05 12:43:31 -0800

    Change the limit of phone numbers to 8 and add other informations needed to sign in form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105181424) do
+ActiveRecord::Schema.define(version: 20180105203515) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20180105181424) do
     t.string "first_name"
     t.string "last_name"
     t.string "email"
-    t.integer "phone_number"
+    t.bigint "phone_number"
     t.string "password_digest"
     t.integer "zipcode"
     t.string "state"
