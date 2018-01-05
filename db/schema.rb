fb5e198a7bb4dc55a056b716a569cd7f96e3dfdd
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-05 10:17:13 -0800

    Add availability,age range,age and identification to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180105015642) do
+ActiveRecord::Schema.define(version: 20180105181424) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,10 @@ ActiveRecord::Schema.define(version: 20180105015642) do
     t.string "verification_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "availability"
+    t.string "identification"
+    t.integer "age"
+    t.string "age_range"
   end
 
 end
