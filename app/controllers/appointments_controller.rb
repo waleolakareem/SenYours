b951b10ed19e8618712167af43659037bda98ccd
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-08 12:34:08 -0800

    Add edit page to edit appointment between users and also styled notification class

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -20,19 +20,33 @@ class AppointmentsController < ApplicationController
   end
 
   def edit
-
+    @appointment = Appointment.find(params[:id])
+    @senior = @appointment.senior
+    @companion = @appointment.companion
   end
 
+  def update
+    @appointment = Appointment.find(params[:id])
+    if @appointment.accept === "false"
+      @appointment.destroy
+      redirect_to user_path(current_user)
+    elsif @appointment.update_attributes(appointment_params)
+      redirect_to user_path(current_user)
+    else
+      render 'edit'
+    end
+  end
   def show
 
   end
 
   def destroy
-
+    @appointment = Appointment.find(params[:id])
+    @appointment.destroy
   end
 
   private
     def appointment_params
-      params.require(:appointment).permit( :start_time, :end_time, :start_date, :end_date, :senior_id, :companion_id, :fee )
+      params.require(:appointment).permit( :start_time, :end_time, :start_date, :end_date, :senior_id, :companion_id, :fee, :accept )
     end
 end
