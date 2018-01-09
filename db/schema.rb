d47becf823a03794446abc156de76147764df12a
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-09 15:21:56 -0800

    Create tables for available_times and available_days and linked necessary models together

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180108233814) do
+ActiveRecord::Schema.define(version: 20180109230022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,22 @@ ActiveRecord::Schema.define(version: 20180108233814) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "accept"
+  end
+
+  create_table "available_days", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "comment"
+    t.date "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "available_times", force: :cascade do |t|
+    t.integer "available_day_id"
+    t.text "comment"
+    t.time "time"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
