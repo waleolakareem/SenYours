a5c5dfc177369c9f5a7fb2662a885244842e5c2e
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-08 19:14:11 -0800

    Move update profile to navbar ensure that seniors can rate companions

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :sessions
   resources :users do
     resources :appointments, shallow: true
+    resources :reviews,shallow: true
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
