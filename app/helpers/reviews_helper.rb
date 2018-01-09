d33ad6b351d775249d99c73f8b80b90290b5a3e1
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-01-09 11:32:31 -0800

    Finish final rating method for review and move method from application_controller to users_helper

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,15 +1,18 @@
 module ReviewsHelper
   def individaul_rating(review)
+    # Add up all the ratings of a review and round it to two
     total_rating = review.comp_rating + review.comm_rating + review.wyr_rating
     average_rating = total_rating/3.to_f
     average_rating.round(2)
   end
 
-  # def final_rating
-  #   y = 0
-  #   current_user.reviews.each do |review|
-  #     y += individaul_rating(review)
-  #   end
-
-  # end
+  def final_rating(user)
+    # Add up all review and divide it by the total count
+    collector = 0
+    user.reviews.each do |review|
+      collector += individaul_rating(review)
+    end
+    rating = collector/user.reviews.count.to_f
+    ratinfg.round(2)
+  end
 end
