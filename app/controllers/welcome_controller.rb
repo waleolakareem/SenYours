40c6de92aa3e474a570cc0ef24c8d3ba135495ef
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2018-02-02 16:14:43 -0800

    Style sign up and sign in page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @arr = ["/assets/companion2.jpg","/assets/companion1.jpg", "/assets/companion4.jpg"];
+    @arr = ["/assets/resize.jpg","/assets/companion2.jpg", "/assets/companion4.jpg"];
   end
 
   def be_a_comp
