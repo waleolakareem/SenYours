587920571f8e74f5526f3597e7c42416f0a3480d
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro-2.local>
2018-03-14 15:16:19 -0700

    add cache to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #cache
+  config.public_file_server.headers = {
+    'Cache-Control' => 'public, s-maxage=31536000, maxage=15552000',
+    'Expires' => "#{1.year.from_now.to_formatted_s(:rfc822)}"
+  }
 end
