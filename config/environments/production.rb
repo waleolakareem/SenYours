8014ad775cd62230d0a41551ccc95d0125f3c110
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro-2.local>
2018-03-13 20:31:24 -0700

    change to false

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,10 +28,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
