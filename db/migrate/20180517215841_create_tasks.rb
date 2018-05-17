b62a286c0275ca30a5b4493c317a6370ad1fa2e7
Amber Nash <nash.beram@gmail.com>
2018-05-17 15:28:32 -0700

    Add task table fields

--- db/migrate/20180517215841_create_tasks.rb
+++ db/migrate/20180517215841_create_tasks.rb
@@ -1,7 +1,15 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-
+      t.boolean :errands, :default => false
+      t.boolean :companionship, :default => false
+      t.boolean :doctor_appointment, :default => false
+      t.boolean :gardening, :default => false
+      t.boolean :preparing_meals, :default => false
+      t.boolean :pet_care, :default => false
+      t.boolean :shopping, :default => false
+      t.boolean :transportation, :default => false
+      
       t.timestamps
     end
   end
