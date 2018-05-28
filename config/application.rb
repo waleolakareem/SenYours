fb4f88554fcc6cf9512a95f8548c971add527da6
Blaine Anderson <BlaineA97@Gmail.com>
2018-05-28 15:07:23 -0700

    ckeditor is functional with submissions

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,8 @@ module SeenYours
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
