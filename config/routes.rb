8cb52b1183acb8f69922ef22bb3fffd4a6039a18
Blaine Anderson <BlaineA97@Gmail.com>
2018-05-28 14:35:13 -0700

    installed ckeditor gem. Updated migration file to have ActiveRecord version of 5.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  mount Ckeditor::Engine => '/ckeditor'
   resources :blog
 
   get 'password_resets/new'
--- db/schema.rb
+++ db/schema.rb
@@ -54,6 +54,18 @@ ActiveRecord::Schema.define(version: 20180528182540) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "ckeditor_assets", force: :cascade do |t|
+    t.string "data_file_name", null: false
+    t.string "data_content_type"
+    t.integer "data_file_size"
+    t.string "type", limit: 30
+    t.integer "width"
+    t.integer "height"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type"], name: "index_ckeditor_assets_on_type"
+  end
+
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
     t.integer "recipient_id"
