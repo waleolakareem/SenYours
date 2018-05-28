1fc3d3e4381dac6039aef523e1a6b4b3a3f793c8
Blaine Anderson <BlaineA97@Gmail.com>
2018-05-28 14:10:42 -0700

    Added basic form submission with minimal design.

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -9,10 +9,28 @@ class BlogController < ApplicationController
     redirect_to :root, alert: 'Blog not found'
   end
 
+  def password
+    #code
+  end
+
   def new
+    @blog = Blog.new
+
+    # if correct_password
+    #   # Entry to create a blog
+    # else
+    #   # Back to index with messages
+    # end
   end
 
   def create
+    @blog = Blog.new(blog_params)
+    if @blog.save
+      flash[:success] = "Blog created succesfully"
+      redirect_to @blog
+    else
+      render new_user_path
+    end
   end
 
   def edit
@@ -20,4 +38,10 @@ class BlogController < ApplicationController
 
   def delete
   end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title, :body)
+    end
+
 end
