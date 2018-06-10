14b72e3e4b375df21dcacf44f61c621cbbf98c2d
Blaine Anderson <BlaineA97@Gmail.com>
2018-06-10 08:57:27 -0700

    Container size no longer fluctuates when Ajax is used.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -11,19 +11,16 @@ class TasksController < ApplicationController
             format.html { redirect_to user_path(@user) }
             format.js { render :index, :locals => {:user_id => params[:user_id], :task_id => params[:task_id]} }
         end
-        # redirect_to user_path(@user)
     end
 
     def remove_selected
         task_id = params[:task_id]
         User.find(params[:user_id]).tasks.destroy(task_id.to_i)
         @user = User.find(params[:user_id])
-
         respond_to do |format|
             format.html { redirect_to user_path(@user) }
             format.js { render :index, :locals => {:user_id => params[:user_id], :task_id => params[:task_id]} }
         end
-        # redirect_to user_path(@user)
     end
 
 end
