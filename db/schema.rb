d52fae4e817be237faf1e2faa3123c2ff82a42e6
Shannon Nolan <shannonnolan17@gmail.com>
2018-06-20 09:58:06 -0700

    Add view layout

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180606202119) do
+ActiveRecord::Schema.define(version: 20180620163853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "about_us", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "appointments", force: :cascade do |t|
     t.integer "senior_id"
