dd6e61f9893d8fe062d8f6c3dbf561b1b47effc3
Oladejo <wale@senyours.io>
2018-06-19 20:34:55 -0700

    remove the bundle gem 12 file

--- config/application.rb
+++ config/application.rb
@@ -15,10 +15,6 @@ module SeenYours
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     
-    config.assets.precompile += %w( images/* )
-    config.assets.precompile += %w( slick/* )
-    config.assets.precompile += %w( stylesheets/* )
-    config.assets.precompile += %w( javascripts/* )
     config.assets.precompile += Ckeditor.assets
     config.assets.precompile += %w( ckeditor/* )
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
