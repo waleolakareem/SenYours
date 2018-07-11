78a1c6473b1079eac75352e7ae61b92eaaaa95e5
Blaine Anderson <BlaineAndersonDev@gmail.com>
2018-07-10 19:35:22 -0700

    Removed unused code lines, cleaned up code

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -29,8 +29,7 @@ class TransactionsController < ApplicationController
   def stripe_webhook
     # To properly test this using the test functionality provided by Stripe, you must use 'ngrok' and add the Webhook endpoint through the tunnel generated.
     event_json = JSON.parse(request.body.read)
-    uri = "https://hooks.slack.com/services/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"
-    RestClient.post uri, {
+    RestClient.post "https://hooks.slack.com/services/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", {
       "attachments": [
           {
               "fallback": "#{event_json}",
@@ -55,7 +54,6 @@ class TransactionsController < ApplicationController
           }
       ]
     }.to_json, {content_type: :json, accept: :json}
-    puts "#{event_json}"
     render status: 200
   end
 
