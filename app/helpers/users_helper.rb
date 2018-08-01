47b4caeb9a196d0b376d3b51c23b2ab52408cb56
Blaine Anderson <BlaineAndersonDev@gmail.com>
2018-07-31 17:22:17 -0700

    Adjusted name of account_sid to TWILLO_ACCOUNT_SID. Adjusted name of auth_token to TWILLO_AUTHORIZATION_TOKEN

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -83,8 +83,8 @@ module UsersHelper
     require 'twilio-ruby'
 
     # put your own credentials here
-    account_sid = ENV['account_sid']
-    auth_token = ENV['auth_token']
+    account_sid = ENV['TWILLO_ACCOUNT_SID']
+    auth_token = ENV['TWILLO_AUTHORIZATION_TOKEN']
 
     # set up a client to talk to the Twilio REST API
     @client = Twilio::REST::Client.new(account_sid, auth_token)
