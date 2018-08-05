502a058d2db163a2d26716dfafea0142c3a54fc6
Blaine Anderson <BlaineAndersonDev@gmail.com>
2018-08-04 19:46:06 -0700

    Added times array and available day parameter to time_sheet route. Route works perfectly using old code. Working to consolidate.

--- app/controllers/available_days_controller.rb
+++ app/controllers/available_days_controller.rb
@@ -16,6 +16,8 @@ class AvailableDaysController < ApplicationController
   end
 
   def time_sheet
+    @availableDay = AvailableDay.where({user_id:current_user,date: available_days_params[:date]})
+    @all_times = ["07:00AM","08:00AM","09:00AM","10:00AM","11:00AM","12:00PM","01:00PM","02:00PM","03:00PM","04:00PM","05:00PM","06:00PM","07:00PM"]
     # Opens specific dates Timesheet
   end
 
