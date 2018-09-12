511d2326826cabcf19a38cd18d765afdfad88fbf
Anna J <ajeszeck@gmail.com>
2018-09-12 12:47:15 -0700

    Add done button to close tasks menu.

--- app/assets/javascripts/tasks_index.js
+++ app/assets/javascripts/tasks_index.js
@@ -1,6 +1,5 @@
 $(document).on('turbolinks:load', function() {
   // This onClick event handler toggles the arrow in the tasks section from down to up, while also hiding and revealing the full list of tasks.
-  console.log("ready");
   $(document).on("click", ".collapsible", function(event) {
       var tasks = this.nextElementSibling;
       $(tasks).slideToggle('fast');
@@ -9,4 +8,13 @@ $(document).on('turbolinks:load', function() {
       downArrow.style.display = (downArrow.style.display === 'none' ? 'block' : 'none');
       upArrow.style.display = (upArrow.style.display === 'none' ? 'block' : 'none');
   });
+
+  $(document).on("click", "#done_button", function(event) {
+    var tasks = $(this).closest(".task-content");
+    $(tasks).slideToggle('fast');
+    var downArrow = $(".task-arrow")[0];
+    var upArrow = $(".task-arrow")[1];
+    downArrow.style.display = (downArrow.style.display === 'none' ? 'block' : 'none');
+    upArrow.style.display = (upArrow.style.display === 'none' ? 'block' : 'none');
+  })
 });
