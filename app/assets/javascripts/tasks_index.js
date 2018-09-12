49466470485e9c6a6671cf7439b894897a228c11
Anna J <ajeszeck@gmail.com>
2018-09-12 09:49:14 -0700

    Arrow indicating collapsibility now flips when drawer is open/closed.

--- app/assets/javascripts/tasks_index.js
+++ app/assets/javascripts/tasks_index.js
@@ -2,8 +2,11 @@ $(document).on('turbolinks:load', function() {
   // This onClick event handler toggles the arrow in the tasks section from down to up, while also hiding and revealing the full list of tasks.
   console.log("ready");
   $(document).on("click", ".collapsible", function(event) {
-      console.log(this);
       var tasks = this.nextElementSibling;
       $(tasks).slideToggle('fast');
+      var downArrow = $(".task-arrow")[0];
+      var upArrow = $(".task-arrow")[1];
+      downArrow.style.display = (downArrow.style.display === 'none' ? 'block' : 'none');
+      upArrow.style.display = (upArrow.style.display === 'none' ? 'block' : 'none');
   });
 });
