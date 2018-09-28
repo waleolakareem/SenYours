bdb21aa430031f88d4876648484f06e5ddee9c9e
Amber Nash <nash.beram@gmail.com>
2018-09-28 02:09:53 -0700

    Secondary styling of profile.

--- app/assets/javascripts/comp_show.js
+++ app/assets/javascripts/comp_show.js
@@ -7,3 +7,19 @@ $("i").hover(function() {
     $(".comp_description").hide();
  })
 });
+
+// On-click attempts:
+
+// $(document).on('click', 'profile_drop', function(){
+//   $(".comp_description").show();
+// });
+//
+//
+// $(document).ready(function() {
+//
+// $("profile_drop").click(function() {
+//     $(".comp_description").show();
+//   }, function() {
+//     $(".comp_description").hide();
+//  })
+// });
